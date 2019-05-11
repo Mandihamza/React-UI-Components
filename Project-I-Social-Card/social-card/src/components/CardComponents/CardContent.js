9d43533640eddd748a700a4b6e4cfaeafb7fa00d
mandihamza <mandihamza@gmail.com>
2019-05-11 15:01:06 -0600

    Built and styled card component and modified header style

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <a href="https://reactjs.org/">
+        <div className='card-content'>
+            <h1>Get Started with React</h1>
+            <p>React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+            <a href="https://reactjs.org/" className="link">reactjs.org</a>
+        </div>
+        </a>
+
+
+        
+    );
+};
+
+export default CardContent;
